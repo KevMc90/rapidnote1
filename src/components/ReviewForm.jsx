e0241e9c805f6f096985ac5e5296897a122b20a8
KevMc90 <kvnmcguire.1990@gmail.com>
2025-04-20 19:04:36 -0500

    Update ReviewForm.jsx

--- src/components/ReviewForm.jsx
+++ src/components/ReviewForm.jsx
@@ -1,11 +1,11 @@
 // rapidnote-frontend/src/components/ReviewForm.jsx
 
 import { useState } from "react";
-import { Textarea } from "@/components/ui/textarea";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import { Label } from "@/components/ui/label";
-import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
+import { Textarea } from "../ui/textarea";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { Label } from "../ui/label";
+import { ToggleGroup, ToggleGroupItem } from "../ui/toggle-group";
 import ReviewOutput from "./ReviewOutput";
 import FileUpload from "./FileUpload";
 
