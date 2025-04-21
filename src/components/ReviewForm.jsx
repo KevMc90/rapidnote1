4dfcdf4cb569bdc735d50839575a7a651d4ea4aa
KevMc90 <kvnmcguire.1990@gmail.com>
2025-04-20 19:53:32 -0500

    Update ReviewForm.jsx

--- src/components/ReviewForm.jsx
+++ src/components/ReviewForm.jsx
@@ -1,10 +1,10 @@
 // ----------- File: /src/components/ReviewForm.jsx -----------
 
-import { Textarea } from "@/components/ui/textarea";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import { Label } from "@/components/ui/label";
-import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
+import { Textarea } from "./ui/textarea";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { Label } from "./ui/label";
+import { ToggleGroup, ToggleGroupItem } from "./ui/toggle-group";
 import ReviewOutput from "./ReviewOutput";
 import FileUpload from "./FileUpload";
 
